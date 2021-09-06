7d3f18da7cb9e6eed3382ee4d732f05df567e16a
Isaac Terry <zeroice2007@gmail.com>
2021-09-05 23:34:41 -0500

    feat: add initial versions of header and nav components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import { Component } from 'react';
+import Nav from '../Nav/Nav';
+
+import './Header.css'
+
+class Header extends Component {
+  state = {
+    isMobileNavShown: false,
+  }
+
+  handleMobileNavBarToggle = () => {
+    this.setState({ isMobileNavShown: !this.state.isMobileNavShown });
+  };
+
+  render() {
+    const { isMobileNavShown } = this.state;
+    const { isSavedNewsHeader } = this.props;
+
+    const btnShownClassName = 'header__menu-btn_shown';
+    const headerClassNames = this.getHeaderClassNames(); 
+
+    return (
+      <header className={headerClassNames}>
+        <div className="header__primary-wrap">
+          <a href="/" className="header__app-name">NewsExplorer</a>
+          <button 
+            className={`header__menu-btn ${isMobileNavShown ?  btnShownClassName : ''}`} 
+            aria-label="Collapse"
+            onClick={this.handleMobileNavBarToggle}
+          >
+            <svg className="header__collapse-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true">
+              <path d="M13.414 12l5.293 5.293-1.414 1.414-6-6a1 1 0 0 1 0-1.414l6-6 1.414 1.414L13.414 12z"/>
+              <path d="M10.879 12l-5.293 5.293L7 18.707l6-6a1 1 0 0 0 0-1.414l-6-6-1.414 1.414L10.879 12z"/>
+            </svg>
+          </button>
+          <button
+            className={`header__menu-btn header__menu-btn_expand-btn ${!isMobileNavShown ? btnShownClassName : ''}`} 
+            aria-label="Expand"
+            onClick={this.handleMobileNavBarToggle}
+          >
+            <span className="header__hamburger-line"></span>
+            <span className="header__hamburger-line"></span>
+          </button>
+        </div>
+        <Nav 
+          isSavedNewsHeader={isSavedNewsHeader} 
+          additionalCssClassNamesStr='header__nav header__nav_shown'    
+        />
+      </header>
+    );
+  }
+
+  getHeaderClassNames() {
+    const { isMobileNavShown } = this.state;
+    const { isSavedNewsHeader } = this.props;
+
+    let headerClassNames = 'header';
+
+    if (isMobileNavShown) {
+      headerClassNames += ' header_expanded';
+
+      if (isSavedNewsHeader) headerClassNames += ' header_saved-news header_state_expanded-saved-news';
+      else headerClassNames += ' header_state_expanded-main';
+
+    } else if (isSavedNewsHeader) headerClassNames += ' header_saved-news';
+    
+    return headerClassNames;
+  }
+}
+
+export default Header;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,44 @@
+import './Nav.css';
+
+function Nav(props) {
+  const { isSavedNewsHeader, additionalCssClassNamesStr='' } = props;
+
+  return (
+    <nav className={`nav ${additionalCssClassNamesStr}`}>
+    <ul className="nav__list">
+      <li className="nav__list-item">
+        <a href="/" className="nav__list-link nav__list-link_type_home">
+          Home
+        </a>
+      </li>
+      { !isSavedNewsHeader && (
+        <li className="nav__list-item nav__list-item_type_sign-in">
+          <a href="/" className="nav__list-link nav__list-link_button nav__list-link_type_sign-in">
+            Sign in
+          </a>
+        </li>
+      )}
+      { isSavedNewsHeader && (
+        <>
+          <li className="nav__list-item nav__list-item_type_saved-news">
+            <a href="/" className="nav__list-link nav__list-link_type_saved-news">
+              Saved articles
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="/" className="nav__list-link nav__list-link_button nav__list-link_type_logout">
+              Elise
+              <svg className="nav__logout-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true" >
+                <path fillRule="evenodd" d="M10 6H6v12h4v2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h4v2zm7.586 7l-4.293 4.134 1.414 1.362 6.707-6.459-6.707-6.459-1.414 1.362 4.293 4.134H8V13h9.586z"/>
+              </svg>
+            </a>
+          </li>
+        </>
+      )}
+    </ul>
+  </nav>
+  );
+}
+
+export default Nav;
+
